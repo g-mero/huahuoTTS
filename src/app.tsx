249571be08cdbfc0bfa7fa78d3cd53d07f462e28
Gmero <2431775986@qq.com>
2024-07-11 02:06:46 +0800

    route animation

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,46 @@
 import '@unocss/reset/tailwind.css'
+import '@unocss/reset/tailwind-compat.css'
 import 'virtual:uno.css'
 
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
-import { Suspense } from 'solid-js'
+import { Suspense, children } from 'solid-js'
+import { Motion, Presence } from 'solid-motionone'
 import Nav from '~/components/Nav'
 
 export default function App() {
+  const NewFileRoutes = () => FileRoutes().map((route) => {
+    return {
+      ...route,
+      component: (props: any) => {
+        return (
+
+          <Motion
+            animate={{ opacity: [0, 1] }}
+            exit={{ opacity: 0 }}
+            transition={{
+              duration: 0.2,
+            }}
+          >
+            <route.component {...props} />
+          </Motion>
+
+        )
+      },
+    }
+  })
   return (
     <Router
-      root={props => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
+      root={(props) => {
+        return (
+          <>
+            <Nav />
+            <Suspense><Presence exitBeforeEnter>{props.children}</Presence></Suspense>
+          </>
+        )
+      }}
     >
-      <FileRoutes />
+      <NewFileRoutes />
     </Router>
   )
 }
